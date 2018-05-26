5dff26df4a5a0887d3db44c9b3be7a8efa12e570
Matheus Abreu <matheusa596@gmail.com>
2018-05-26 08:42:01 -0300

    Criação da classe de Mensagem que será trocada entre os processos.

---
+++ thread_teste/src/thread_teste/Mensagem.java
@@ -0,0 +1,35 @@
+package thread_teste;
+
+import java.io.Serializable;
+
+public class Mensagem implements Serializable{
+	
+	private int pidOrigem;
+	private TipoMensagem tipoMensagem;
+	private int portaPid;
+	
+	public enum TipoMensagem {
+		ELEICAO, OK, COORDENADOR, BERKLEY
+	}
+	
+	public Mensagem (int pid, int portaPid, TipoMensagem tipoMensagem) {
+		this.pidOrigem = pid;
+		this.tipoMensagem = tipoMensagem;
+		this.portaPid = portaPid;
+	}
+	
+	public int getPidOrigem () {
+		return this.pidOrigem;
+	}
+	
+	public TipoMensagem getTipoMensagem () {
+		return this.tipoMensagem;
+	}
+
+	@Override
+	public String toString() {
+		return "Mensagem [pidOrigem=" + pidOrigem + ", tipoMensagem=" + tipoMensagem + "]";
+	}
+	
+	
+}
